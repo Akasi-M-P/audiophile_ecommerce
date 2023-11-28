16f0c3d5d16d5dc1eda37d77fb0ea001dffbc629
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2023-11-28 22:55:21 +0000

    update

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -3,18 +3,18 @@ import "./Home.css";
 const Home = () => {
   return (
     <main className="hero">
-      <section className="w-2/3 mx-auto flex flex-col items-center justify-center h-full gap-14 pt-28">
-        <p className="text-2xl wide-spacing text-gray-400">NEW PRODUCT</p>
-        <h1 className="text-6xl font-extrabold uppercase text-white">
+      <section className="w-2/3 mx-auto flex flex-col items-center justify-center gap-4 absolute top-72 left-16">
+        <p className=" wide-spacing text-gray-400 font-light">NEW PRODUCT</p>
+        <h1 className="text-4xl font-extrabold uppercase text-white">
           XX99 Mark II <br />
           HeadphoneS
         </h1>
-        <p className="text-2xl text-gray-300 text-center">
+        <p className="text-sm text-gray-300 text-center">
           Experience natural, lifelike audio and exceptional build quality made
           for the passionate music enthusiast.
         </p>
-        <button type="" className="px-8 py-5 bg-orange-400">
-          <p className="text-xl uppercase text-white ">See Product</p>
+        <button type="" className="px-5 py-2 bg-orange-400">
+          <p className="text-sm uppercase text-white ">See Product</p>
         </button>
       </section>
     </main>
