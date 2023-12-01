4e33f308d1a88c05bc7970bfb27e8bdade2d4d30
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2023-12-01 20:00:07 +0000

    done working on mobile home page

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import "./Home.css";
 import { IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { FaFacebookSquare, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Home = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -138,6 +139,109 @@ const Home = () => {
           </button>
         </div>
       </section>
+      <section className="w-3/4 mx-auto relative rounded-md mb-5 orange-container-2">
+        <h1 className="absolute top-24 left-9 text-2xl font-extrabold">
+          ZX7 SPEAKER
+        </h1>
+        <button
+          type=""
+          className="uppercase absolute text-sm bottom-24 left-9 px-4 py-4 border border-black"
+        >
+          see product
+        </button>
+      </section>
+      <section className="w-10/12 mx-auto relative  mb-5">
+        <img
+          src="/resources/assets/home/mobile/image-earphones-yx1.jpg"
+          alt=""
+          className="w-full rounded-lg"
+        />
+      </section>
+      <section className="w-10/12 md:w-1/3 lg:w-1/3  h-48 xl:h-64 mx-auto rounded-8 bg-gray-200 relative rounded-md mb-32 relative">
+        <h1 className="absolute top-10 left-9 text-2xl font-extrabold">
+          YX1 EARPHONES
+        </h1>
+        <button
+          type=""
+          className="uppercase absolute text-sm bottom-10 left-9 px-4 py-4 border border-black"
+        >
+          see product
+        </button>
+      </section>
+      <section className="mb-32">
+        <img
+          src="/assets/Bitmap-manHP.png"
+          alt=""
+          className="w-10/12 mx-auto mb-10"
+        />
+        <div className="w-10/12 mx-auto mb-5">
+          <h1 className="uppercase text-black text-3xl font-extrabold text-center mb-10">
+            Bringing you the <span className="text-orange-400">best</span> audio
+            gear
+          </h1>
+          <p className="text-gray-400 text-center px-2">
+            Located at the heart of New York City, Audiophile is the premier
+            store for high end headphones, earphones, speakers, and audio
+            accessories. We have a large showroom and luxury demonstration rooms
+            available for you to browse and experience a wide range of our
+            products. Stop by our store to meet some of the fantastic people who
+            make Audiophile the best place to buy your portable audio equipment.
+          </p>
+        </div>
+      </section>
+      <section className="w-10/12 mx-auto bg-black mb-20 py-10">
+        <nav className="w-full items-center  ">
+          {/* ... your existing desktop navigation code ... */}
+          <div className="mb-12">
+            <Link to="/">
+              <p className="text-2xl font-extrabold text-center text-white">
+                audiophile
+              </p>
+            </Link>
+          </div>
+          <div className="flex flex-col text-white text-sm items-center justify-center gap-10">
+            <div>
+              <Link to="/">
+                <p className="uppercase hover:text-orange-500">home</p>
+              </Link>
+            </div>
+            <div>
+              <Link to="/headphones">
+                <p className="uppercase hover:text-orange-500">headphones</p>
+              </Link>
+            </div>
+            <div>
+              <Link to="/speakers">
+                <p className="uppercase hover:text-orange-500">speakers</p>
+              </Link>
+            </div>
+            <div>
+              <Link to="/earphones">
+                <p className="uppercase hover:text-orange-500">earphones</p>
+              </Link>
+            </div>
+          </div>
+        </nav>
+        <div className="py-10 px-2">
+          <p className="text-gray-400 text-center text-sm">
+            Audiophile is an all in one stop to fulfill your audio needs. We're
+            a small team of music lovers and sound specialists who are devoted
+            to helping you get the most out of personal audio. Come and visit
+            our demo facility - we’re open 7 days a week.
+          </p>
+        </div>
+        <div className="w-10/12 mx-auto flex flex-col gap-10">
+          <div>
+            <p className="text-gray-400">Copyright 2021. All Rights Reserved</p>
+          </div>
+          <div className="text-white flex items-center justify-center gap-5">
+            <FaFacebookSquare className="w-6 h-6" />
+            <FaTwitter className="w-6 h-6" />
+            <FaInstagram className="w-6 h-6" />
+          </div>
+        </div>
+      </section>
+
       <section>{/* {isMobile && ()} */}</section>
     </>
   );
