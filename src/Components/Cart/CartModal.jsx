b01f443b06f93221e798617d1dc11ba3b08ec733
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2023-12-04 16:52:47 +0000

    done with cart ui

--- src/Components/Cart/CartModal.jsx
+++ src/Components/Cart/CartModal.jsx
@@ -38,30 +38,46 @@ const CartModal = ({
 
   return (
     <div
-      className="cart-modal-overlay fixed inset-0 bg-black bg-opacity-50 z-10"
+      className="cart-modal-overlay fixed inset-0 bg-black bg-opacity-30 z-10 "
       onClick={handleOverlayClick}
     >
-      <div className="cart-modal w-10/12 bg-white p-4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div className="cart-modal w-11/12 bg-white p-4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg">
         <div className="flex justify-end">
           <button className="text-red-500 hover:text-red-700" onClick={onClose}>
-            Close
+            x
           </button>
         </div>
-        <h2 className="text-black text-2xl mb-4">Your Cart</h2>
 
         <div>
-          <p>Number of items in the cart: {cart ? cart.length : 0}</p>
+          <div className="flex justify-between">
+            <h1 className="uppercase font-extrabold">
+              cart ({cart ? cart.length : 0})
+            </h1>
+            <button onClick={onRemoveAll} className="text-sm text-gray-400">
+              Remove All
+            </button>
+          </div>
 
-          <button onClick={onRemoveAll}>Remove All</button>
-          <ul>
+          <div>
             {cart &&
               Array.isArray(cart) &&
               cart.map((item, index) => (
-                <li key={index}>
-                  <img src={item.image.mobile} alt="" />
-                  {item.name} - Quantity: {quantities[item.id]}
-                  <p>$ {item.price}</p>
-                  <div className="w-32 border border-transparent bg-gray-200 pl-3">
+                <div key={index} className="flex items-center gap-2 mb-5 py-5">
+                  <div>
+                    <img
+                      src={item.image.mobile}
+                      alt=""
+                      className="w-20 h-16 rounded-md"
+                    />
+                  </div>
+                  <div>
+                    <h1 className="text-sm font-extrabold">{item.name}</h1>
+                    <p className="text-sm font-extrabold text-gray-400">
+                      $ {item.price}
+                    </p>
+                  </div>
+
+                  <div className="w-24 h-10  bg-gray-200 border border-transparent rounded-sm  text-sm text-center">
                     <button
                       onClick={() =>
                         handleQuantityChange(item.id, quantities[item.id] - 1)
@@ -81,7 +97,7 @@ const CartModal = ({
                           parseInt(e.target.value, 10)
                         )
                       }
-                      className=" w-20 px-2 py-2 text-center text-sm bg-gray-200"
+                      className=" w-12 px-2 py-2 text-center text-sm bg-gray-200"
                     />
                     <button
                       onClick={() =>
@@ -91,12 +107,31 @@ const CartModal = ({
                       +
                     </button>
                   </div>
-                  <button onClick={() => onRemoveItem(item.id)}>Remove</button>
-                </li>
+                  <div className="w-5 flex items-center ">
+                    <button
+                      onClick={() => onRemoveItem(item.id)}
+                      className="text-sm flex items-center "
+                    >
+                      x
+                    </button>
+                  </div>
+                </div>
               ))}
-          </ul>
-          <p>Total Price: ${getTotalPrice().toLocaleString()}</p>
-          <button onClick={onCheckout}>Checkout</button>
+          </div>
+          <div className="flex justify-between">
+            <p className="uppercase text-gray-400">Total </p>
+            <p className="font-extrabold">
+              $ {getTotalPrice().toLocaleString()}
+            </p>
+          </div>
+          <div className="w-full mx-auto flex items-center justify-center">
+            <button
+              onClick={onCheckout}
+              className="bg-orange-400 text-white w-full px-4 py-2"
+            >
+              Checkout
+            </button>
+          </div>
         </div>
       </div>
     </div>
